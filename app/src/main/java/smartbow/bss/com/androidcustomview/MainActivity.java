86aba3d6dcd24942e1333905bffd233fd6176201
lijiakui <lijk@smartbow.net>
2016-06-28 11:02:54 +0800

    first commit

---
+++ app/src/main/java/smartbow/bss/com/androidcustomview/MainActivity.java
@@ -0,0 +1,13 @@
+package smartbow.bss.com.androidcustomview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
